2a87b129013f927cc54a243f5f9275b395ad67ea
Jarrod Parkes <jarrod@udacity.com>
2015-04-07 13:32:10 -0700

    making changes for step 2.3

--- FlickFinder/FlickFinder/ViewController.swift
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flickr.photos.search"
+let API_KEY = "ENTER_YOUR_API_KEY_HERE"
+let EXTRAS = "url_m"
+let SAFE_SEARCH = "1"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var photoImageView: UIImageView!
@@ -18,7 +26,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var longitudeTextField: UITextField!
 
     @IBAction func searchPhotosByPhraseButtonTouchUp(sender: AnyObject) {
-        println("Implement this function...")
+        /* 1 - Hardcode the arguments */
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "text": "baby asian elephant",
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        /* 2 - Call the Flickr API with these arguments */
+        getImageFromFlickrBySearch(methodArguments)
     }
     
     @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
@@ -35,5 +54,51 @@ class ViewController: UIViewController {
 
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
+    }
+    
+    func getImageFromFlickrBySearch(methodArguments: [String : AnyObject]) {
+        
+        /* 3 - Get the shared NSURLSession to faciliate network activity */
+        let session = NSURLSession.sharedSession()
+        
+        /* 4 - Create the NSURLRequest using properly escaped URL */
+        let urlString = BASE_URL + escapedParameters(methodArguments)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        /* 5 - Create NSURLSessionDataTask and completion handler */
+        let task = session.dataTaskWithRequest(request) {data, response, downloadError in
+            if let error = downloadError? {
+                println("Could not complete the request \(error)")
+            } else {
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as NSDictionary
+                println(parsedResult.valueForKey("photos"))
+            }
+        }
+        
+        /* 6 - Resume (execute) the task */
+        task.resume()
+    }
+    
+    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(escapedValue!)"]
+            
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
     }
 }
