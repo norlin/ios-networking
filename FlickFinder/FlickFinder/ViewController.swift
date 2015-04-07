708082c2b8d7eb82c99720bfbc60e023e4bf6e0a
Jarrod Parkes <jarrod@udacity.com>
2015-04-07 13:18:20 -0700

    making changes for step 2.1

---
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  FlickFinder
+//
+//  Created by Jarrod Parkes on 1/29/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var defaultLabel: UILabel!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+}
