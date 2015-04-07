b826429cdc6af55b43d1080e42dd4339d8e58194
Jarrod Parkes <jarrod@udacity.com>
2015-04-07 14:53:05 -0700

    making changes for step 2.8

--- FlickFinder/FlickFinder/ViewController.swift
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -155,6 +155,14 @@ class ViewController: UIViewController {
         return "\(bottom_left_lon),\(bottom_left_lat),\(top_right_lon),\(top_right_lat)"
     }
 
+    func getLatLonString() -> String {
+        let latitude = (self.latitudeTextField.text as NSString).doubleValue
+        let longitude = (self.longitudeTextField.text as NSString).doubleValue
+        
+        return "(\(latitude), \(longitude))"
+    }
+
+    /* Function makes first request to get a random page, then it makes a request to get an image with the random page */
     func getImageFromFlickrBySearch(methodArguments: [String : AnyObject]) {
         
         let session = NSURLSession.sharedSession()
@@ -164,6 +172,46 @@ class ViewController: UIViewController {
         
         let task = session.dataTaskWithRequest(request) {data, response, downloadError in
             if let error = downloadError {
+                println("Could not complete the request \(error)")
+            } else {
+                
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as NSDictionary
+                
+                if let photosDictionary = parsedResult.valueForKey("photos") as? [String:AnyObject] {
+                    
+                    if let totalPages = photosDictionary["pages"] as? Int {
+                        
+                        /* Flickr API - will only return up the 4000 images (100 per page * 40 page max) */
+                        let pageLimit = min(totalPages, 40)
+                        let randomPage = Int(arc4random_uniform(UInt32(pageLimit))) + 1
+                        self.getImageFromFlickrBySearchWithPage(methodArguments, pageNumber: randomPage)
+                        
+                    } else {
+                        println("Cant find key 'pages' in \(photosDictionary)")
+                    }
+                } else {
+                    println("Cant find key 'photos' in \(parsedResult)")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+
+    func getImageFromFlickrBySearchWithPage(methodArguments: [String : AnyObject], pageNumber: Int) {
+        
+        /* Add the page to the method's arguments */
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(withPageDictionary)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        let task = session.dataTaskWithRequest(request) {data, response, downloadError in
+            if let error = downloadError? {
                 println("Could not complete the request \(error)")
             } else {
                 var parsingError: NSError? = nil
@@ -190,7 +238,13 @@ class ViewController: UIViewController {
                                 dispatch_async(dispatch_get_main_queue(), {
                                     self.defaultLabel.alpha = 0.0
                                     self.photoImageView.image = UIImage(data: imageData)
-                                    self.photoTitleLabel.text = "\(photoTitle!)"
+                                    
+                                    if methodArguments["bbox"] != nil {
+                                        self.photoTitleLabel.text = "\(self.getLatLonString()) \(photoTitle!)"
+                                    } else {
+                                        self.photoTitleLabel.text = "\(photoTitle!)"
+                                    }
+                                    
                                 })
                             } else {
                                 println("Image does not exist at \(imageURL)")
