a308ab0440fbd35b0b2346b84157d1a7bccd96ab
Jarrod Parkes <jarrod@udacity.com>
2015-04-08 17:01:47 -0700

    updates for Swift 1.2

--- MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
+++ MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
@@ -125,7 +125,7 @@ class LoginViewController: UIViewController {
         /* 4. Make the request */
         let task = session.dataTaskWithRequest(request) { data, response, downloadError in
             
-            if let error = downloadError? {
+            if let error = downloadError {
                 dispatch_async(dispatch_get_main_queue()) {
                     self.debugTextLabel.text = "Login Failed (Request Token)."
                 }
@@ -134,7 +134,7 @@ class LoginViewController: UIViewController {
                 
                 /* 5. Parse the data */
                 var parsingError: NSError? = nil
-                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as NSDictionary
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
                 
                 /* 6. Use the data! */
                 if let requestToken = parsedResult["request_token"] as? String {
